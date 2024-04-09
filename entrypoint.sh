38826387611d642879a75b961229efdae83b7872
lucaspintos909 <lucaspintos909@gmail.com>
2024-04-09 19:54:58 -0300

    refactor: Sacando chmod duplicado

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-chmod -R 777 ./node_modules
-
 # Para solucionar problema con vite y los node_modules instalados en volumen separado de docker
 # Link: https://github.com/vitejs/vite/issues/2671#issuecomment-829535806
 npm rebuild esbuild
