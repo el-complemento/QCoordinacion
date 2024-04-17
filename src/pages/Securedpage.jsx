747b26b328af2bc774c0a6c11d830949586d268f
lucaspintos909 <lucaspintos909@gmail.com>
2024-04-17 19:12:52 -0300

    feat: Pruebas consultas backend

--- src/pages/Securedpage.jsx
+++ src/pages/Securedpage.jsx
@@ -1,10 +1,31 @@
-const Secured = () => {
+import { useKeycloak } from "@react-keycloak/web";
 
- return (
-   <div>
-     <h1 className="text-green-800 text-4xl">WTas autenticado maquina.</h1>
-   </div>
- );
+const Secured = () => {
+  const { keycloak } = useKeycloak();
+
+  const handleConsultaPrueba = () => {
+    fetch("http://localhost:8080/api/v1/ordenes", {
+      method: "GET", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      //credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + keycloak.token,
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+      });
+  };
+
+  return (
+    <div>
+      <h1 className="text-green-800 text-4xl">WTas autenticado maquina.</h1>
+      <button onClick={handleConsultaPrueba}>Hace click consulta</button>
+    </div>
+  );
 };
 
 export default Secured;
