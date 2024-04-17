6c6a29bfd69c4443d288881594f278b4a07a5241
lucaspintos909 <lucaspintos909@gmail.com>
2024-04-17 19:11:50 -0300

    chore: Entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 # Para solucionar problema con vite y los node_modules instalados en volumen separado de docker
 # Link: https://github.com/vitejs/vite/issues/2671#issuecomment-829535806
 npm rebuild esbuild
-exec "$@"
 chmod -R 777 ./node_modules
+exec "$@"
